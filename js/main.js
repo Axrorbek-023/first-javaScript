7cafa8ace8643819c8a7ef1ae626ba263d7dc20b
Axrorbek-023 <axrorbek2231@gmail.com>
2024-01-19 20:56:56 +0500

    first JavaScript

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Number - 123456789
+// String - ''
+// Underfind
+// Null
+// boolean - true , folse
+// Symbol - symbol('')
+// object - {}
+
+
+
+// var age = prompt('age') 
+// var b = 18 
+// if(age ==  b){ 
+//     console.log('dost'); 
+// }else if(age < b){ 
+//     console.log('uka'); 
+// }else if(age > b){ 
+//     console.log('aka'); 
+
+
+var num = prompt('raqam kiriting')
+if(num == 1){
+    console.log(num ++);
+}
+else(num > 1)
+{
+    console.log(Math.floor(num));
+}
